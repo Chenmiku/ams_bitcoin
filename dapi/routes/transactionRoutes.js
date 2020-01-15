effe5697cbb8232eaf23b1b93a81ddf076bc5552
Chen <vuhongthaihy@gmail.com>
2020-01-15 17:20:02 +0700

    + commit

--- dapi/routes/transactionRoutes.js
+++ dapi/routes/transactionRoutes.js
@@ -7,6 +7,6 @@ const transactionController = require('../api/public/transactionController');
 router.get('/get_all', transactionController.list_all_transaction)
 router.put('/deposit_state_by_address', transactionController.check_deposit_state)
 router.put('/check_transaction', transactionController.check_transaction)
-router.post('/send_to_polebit', transactionController.create_a_transaction)
+router.post('/send_to_bit', transactionController.create_a_transaction)
 
 module.exports = router
