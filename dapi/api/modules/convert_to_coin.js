2e8ff2dac7d08f3d81dc2577bd0dc3cc25585f38
Chen <vuhongthaihy@gmail.com>
2020-01-16 12:01:36 +0700

    + cm

--- dapi/api/modules/convert_to_coin.js
+++ dapi/api/modules/convert_to_coin.js
@@ -4,7 +4,7 @@ exports.convertToCoin = function(coinType , value) {
 	case 'btc':
 		result = parseFloat(value) / 100000000
 	case 'eth':
-		result = parseFloat(value) / 1000000000000000000
+		result = parseFloat(value) / 1000000000000000000n
 	case '':
 		result = parseFloat(value) / 100000000
 	}
