1aa1a16247c4c05b97dfbc1419fdc30a62228079
Chen <vuhongthaihy@gmail.com>
2020-01-16 12:53:20 +0700

    +cm

--- dapi/api/modules/convert_to_coin.js
+++ dapi/api/modules/convert_to_coin.js
@@ -3,16 +3,17 @@ const Web3 = require('web3'),
       w3 = new Web3(new Web3.providers.HttpProvider(mainnet))
 
 exports.convertToCoin = async(coinType , value) => {
-	var result
+	var result = ''
 	switch(coinType) {
 	case 'btc':
-		result = parseFloat(value) / 100000000
+		result = String(parseFloat(value) / 100000000)
 	case 'eth':
 		await w3.utils.fromWei(value, 'ether').then(function(res){
+			console.log(res)
 			result = res
 		})
 	case '':
-		result = parseFloat(value) / 100000000
+		result = String(parseFloat(value) / 100000000)
 	}
 
 	return result
--- dapi/api/public/addressController.js
+++ dapi/api/public/addressController.js
@@ -368,8 +368,8 @@ exports.get_a_address = async(req, res) => {
     } else {
       addressResult.data.addr = addr
       console.log(convert.convertToCoin(coin, ad.balance_string))
-      addressResult.data.balance = String(convert.convertToCoin(coin, ad.balance_string))
-      addressResult.data.unconfirmed_balance = String(convert.convertToCoin(coin, ad.unconfirmed_balance_string))
+      addressResult.data.balance = convert.convertToCoin(coin, ad.balance_string)
+      addressResult.data.unconfirmed_balance = convert.convertToCoin(coin, ad.unconfirmed_balance_string)
       addressResult.data.final_transaction = ad.final_transaction
       addressResult.data.coin_type = coin
       addressResult.data.user_id = ad.user_id || 0
