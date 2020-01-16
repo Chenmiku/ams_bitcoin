5e4913fa4c50e18a2b9249a9c9d825434df66e58
Chen <vuhongthaihy@gmail.com>
2020-01-16 12:59:43 +0700

    +cm

--- dapi/api/modules/convert_to_coin.js
+++ dapi/api/modules/convert_to_coin.js
@@ -2,16 +2,13 @@ const Web3 = require('web3'),
       mainnet = process.env.Provider,
       w3 = new Web3(new Web3.providers.HttpProvider(mainnet))
 
-exports.convertToCoin = async(coinType , value) => {
+exports.convertToCoin = function(coinType , value) {
 	var result = ''
 	switch(coinType) {
 	case 'btc':
 		result = String(parseFloat(value) / 100000000)
 	case 'eth':
-		await w3.utils.fromWei(value, 'ether').then(function(res){
-			console.log(res)
-			result = res
-		})
+		result = w3.utils.fromWei(value, 'ether');
 	case '':
 		result = String(parseFloat(value) / 100000000)
 	}
