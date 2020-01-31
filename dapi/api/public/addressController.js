fc1e0809b5f2f1f1e49bbef05b919da81af358e3
Chen <vuhongthaihy@gmail.com>
2020-01-31 13:26:37 +0700

    +cm

--- dapi/api/public/addressController.js
+++ dapi/api/public/addressController.js
@@ -603,11 +603,12 @@ async function checkDeposit(coin,address,walletName,res,service) {
       });
 
       // get deposit info
-      for(var i = blockNumber-2; i < blockNumber; i++) {
+      for(var i = blockNumber-1; i <= blockNumber; i++) {
         console.log(i)
         await w3.eth.getBlock(i, true).then(function(block){
           for(var j = 0; j < block.transactions; j++) {
             if( block.transactions[j].to == address )
+                console.log('in')
                 value = block.transactions[j].value
                 hash = block.transactions[j].hash
                 console.log(value)
