3a09c4634e645cad0fc4ba518baa3d4bf5e59629
Chen <vuhongthaihy@gmail.com>
2020-04-19 22:10:22 +0700

    h

--- dapi/api/public/transactionController.js
+++ dapi/api/public/transactionController.js
@@ -549,7 +549,7 @@ exports.create_a_transaction = async(req, res) => {
         gasLimit: w3.utils.toHex(200000),
         to: contractAddress,
         value: w3.utils.toHex(0), //senderBalance - feeValue
-        data: contractInstance.methods.transfer(receiver, web3.utils.toHex(100)).encodeABI()
+        data: contractInstance.methods.transfer(receiver, w3.utils.toHex(100)).encodeABI()
       }
 
       var privateKey = new Buffer(addressKey.private_key.substring(2,66), 'hex')
