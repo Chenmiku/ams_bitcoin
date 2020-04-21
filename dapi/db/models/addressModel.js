d3def5a7add5cfc9f1b8fb2deb785374aac29e4f
Chen <vuhongthaihy@gmail.com>
2020-04-21 15:57:47 +0700

    h

--- dapi/db/models/addressModel.js
+++ dapi/db/models/addressModel.js
@@ -43,6 +43,12 @@ var addressSchema = new schema({
     coin_type: {
         type: String
     },
+    // token_type: {
+    //     type: String
+    // },
+    // token_balance: {
+    //     type: Number
+    // },
     ctime: {
         type: String,
     }, 
